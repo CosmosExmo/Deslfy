d89112ed26c25f1eec3167ac522bce805392d921
CosmosExmo <48317781+CosmosExmo@users.noreply.github.com>
2023-06-12 09:22:27 +0300

    fix env path

--- src/start.sh
+++ src/start.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-include src/app.env
-export
-
 set -e
 
 echo "RUN DB Migration"
-
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "START THE APP"
