44cf05d3e9debe52c4982cee4b63198e1e47d5a3
CosmosExmo <48317781+CosmosExmo@users.noreply.github.com>
2023-07-09 22:40:34 +0300

    added update user

--- src/db/sqlc/users_test.go
+++ src/db/sqlc/users_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"desly/util"
 	"testing"
 
@@ -64,4 +65,26 @@ func TestGetUser(t *testing.T) {
 	require.NotEmpty(t, user.HashedPassword)
 	require.NotEmpty(t, user.Email)
 	require.NotEmpty(t, user.FullName)
+}
+
+func TestPartialUpdateUser(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newFullName := util.RandomString(12)
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: oldUser.Username,
+		FullName: sql.NullString{
+			String: newFullName,
+			Valid:  true,
+		},
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, updatedUser.Username, oldUser.Username)
+	require.Equal(t, updatedUser.Email, oldUser.Email)
+	require.Equal(t, updatedUser.HashedPassword, oldUser.HashedPassword)
+
+	require.Equal(t, updatedUser.FullName, newFullName)
+	require.NotEqual(t, updatedUser.FullName, oldUser.FullName)
 }
