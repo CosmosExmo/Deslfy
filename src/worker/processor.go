5a0adfcde0e3c06d432421a3fead18c82f5d6273
CosmosExmo <48317781+CosmosExmo@users.noreply.github.com>
2023-07-14 16:36:11 +0300

    added email sender and verify email logic

--- src/worker/processor.go
+++ src/worker/processor.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"context"
 	db "desly/db/sqlc"
+	"desly/mail"
 
 	"github.com/hibiken/asynq"
 	"github.com/redis/go-redis/v9"
@@ -25,9 +26,10 @@ type TaskProcessor interface {
 type RedisTaskProcessor struct {
 	server *asynq.Server
 	store  db.Store
+	mailer mail.EmailSender
 }
 
-func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store, mailer mail.EmailSender) TaskProcessor {
 	logger := NewLogger()
 	redis.SetLogger(logger)
 
@@ -45,6 +47,7 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 	return &RedisTaskProcessor{
 		server: server,
 		store:  store,
+		mailer: mailer,
 	}
 }
 
